cbf41f9b23bd7de11641f38316596b91adce69f5
Alex <alexmengue1990@hotmail.com>
2023-02-21 17:06:09 -0300

    added score-system and a game over message

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ window.onload = () => {
     let stage = document.getElementById('stage');
     let context = stage.getContext('2d');
 
+    let currentScore = document.getElementById('current-score');
+    let topScore = document.getElementById('top-score');
+    
+    let gameOver = document.getElementById('game-over');
+
     document.addEventListener('keydown', keyPush);
 
     setInterval(game, 60);
@@ -11,17 +16,19 @@ window.onload = () => {
     let speedX = 0;
     let speedY = 0;
 
-    let positionX = 10;
-    let positionY = 10;
+    let positionX = 0;
+    let positionY = 0;
 
     let length = 20;
     let count = 20;
 
-    let targetX = 15;
-    let targetY = 15;
+    let targetX = Math.floor(Math.random() * count);
+    let targetY = Math.floor(Math.random() * count);
 
     let trail = [];
-    let tail = 5;
+    let snakeWidth = 5;
+
+    let hasStarted = false;
 
     function game() {
         positionX += speedX;
@@ -46,29 +53,48 @@ window.onload = () => {
         context.fillStyle = 'black';
         context.fillRect(0, 0, stage.clientWidth, stage.height);
 
-        context.fillStyle = 'red';
+        context.fillStyle = 'yellow';
         context.fillRect(targetX * length, targetY * length, length, length);
 
-        context.fillStyle = 'gray';
+        context.fillStyle = 'lightgray';
 
         for (let i = 0; i < trail.length; i++) {
-            context.fillRect(trail[i].x * length, trail[i].y * length, length, length);
+            context.fillRect(trail[i].x * length, trail[i].y * length, length, length);     
 
-            if (trail[i].x === positionX && trail[i].y === positionY) {
-                speedX = 0;
-                speedY = 0;
-                tail = 5;
+            if (hasStarted === true) {
+                if (trail[i].x === positionX && trail[i].y === positionY) {
+                    speedX = 0;
+                    speedY = 0;
+                    
+                    snakeWidth = 5;
+    
+                    positionX = 0;
+                    positionY = 0;
+                
+                    if (Number(currentScore.innerHTML) > Number(topScore.innerHTML)) {
+                        topScore.innerHTML = currentScore.innerHTML;
+                    }
+    
+                    gameOver.style.display = 'block';
+                        setInterval(() => {
+                            gameOver.style.display = 'none';
+                        }, 2000);  
+    
+                    currentScore.innerHTML = 0;
+                    hasStarted = false;
+                }
             }
         }
 
         trail.push({ x: positionX, y:positionY });
 
-        while (trail.length > tail) {
+        while (trail.length > snakeWidth) {
             trail.shift();
         }
 
         if (targetX === positionX && targetY === positionY) {
-            tail++;
+            snakeWidth++;
+            currentScore.innerHTML++;
 
             targetX = Math.floor(Math.random() * count);
             targetY = Math.floor(Math.random() * count);
@@ -80,21 +106,25 @@ window.onload = () => {
             case 37: 
                 speedX = -speed;
                 speedY = 0;
+                hasStarted = true
                 break;
 
             case 38: 
                 speedX = 0;
                 speedY = -speed;
+                hasStarted = true
                 break;
 
             case 39: 
                 speedX = speed;
                 speedY = 0;
+                hasStarted = true
                 break;
 
             case 40: 
                 speedX = 0;
                 speedY = speed;
+                hasStarted = true
                 break;
         }
     }
